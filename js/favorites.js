a6a1d1e58abb60de2109cbb0d8785020661e199c
samuel <samuel.psanttos@gmail.com>
2023-03-23 21:02:16 -0300

    organize code

--- js/favorites.js
+++ js/favorites.js
@@ -42,7 +42,6 @@ export class favorites {
   delete(user) { // Função para deletar o usuário da tabela
     const filteredEntries = this.entries
       .filter(entry => entry.login !== user.login) // Se a lógica "filter" retornar falso, o usuário será removido da tabela.
-      
       this.entries = filteredEntries
       this.update()
       this.save()
@@ -54,7 +53,6 @@ export class favorites {
 export class favoritesView extends favorites {
   constructor(root) {
     super(root)
-
     this.tbody = this.root.querySelector('table tbody') // Pega o elemento tbody do HTML
     this.update()
     this.onadd()
@@ -64,14 +62,12 @@ export class favoritesView extends favorites {
     const buttonSearch = this.root.querySelector(".search button")
     buttonSearch.onclick = () => {
       const { value } = this.root.querySelector(".search input") // Desestruturação de código
-      
       this.add(value)
     }
   }
 
   update() { // Função para atualizar os dados da tabela
     this.removeAllTr()
-
     this.entries.forEach(user => { // Para cada usuário, alterar os dados
       const row = this.createRow()
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
@@ -81,7 +77,6 @@ export class favoritesView extends favorites {
       row.querySelector('.user span').innerText = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
-      
       row.querySelector('.remove').onclick = () => { // Evento do botaão de remover o usuário da tabela
         const isOk = confirm("Tem certeza que deseja remover esse usuário?") // Pop-up de confirmação 
         if(isOk) {
@@ -103,7 +98,6 @@ export class favoritesView extends favorites {
 
   createRow() { // Criar a tag TR e inserir o HTML dentro dela.
     const tr = document.createElement('tr')
-
     tr.innerHTML = `
       <td class="user">
       <img src="https://github.com/samuuuk.png" alt="Foto do dollynho">
