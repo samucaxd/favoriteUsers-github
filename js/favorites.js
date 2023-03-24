3b8c40bd180796e7c324420723b217b3a22b28b4
samuel <samuel.psanttos@gmail.com>
2023-03-23 21:12:50 -0300

    clean code

--- js/favorites.js
+++ js/favorites.js
@@ -1,74 +1,70 @@
 import { githubUser } from "./githubUser.js"
 
-// Criar classe para estruturação de dados
 export class favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
   }
 
-  async add(username) { // Função assíncrona, aguarda um tempo pra rodar | função para adicionar nova linha
-    try { // Tente esse código
-
+  async add(username) { 
+    try {
       const userExists = this.entries
-        .find(entry => entry.login === username) // Verifica se o usuário já está na entries.
+        .find(entry => entry.login === username) 
       if(userExists) {
-        throw new Error('Usuário já cadastrado!') // Caso username já exista, aparecer mensagem de erro
+        throw new Error('Usuário já cadastrado!') 
       }
 
-      const user = await githubUser.search(username) // await => esperar o código inteiro pra depois rodar essa linha
-      if(user.login === undefined) { // Verifica se o usuário não existe no Github
+      const user = await githubUser.search(username)
+      if(user.login === undefined) { 
         throw new Error('Usuário não encontrado!')
       }
 
-      this.entries = [user, ...this.entries] // Adicionar o novo usuário e mantém os demais na tabela
+      this.entries = [user, ...this.entries]
       this.update()
       this.save()
 
-    } catch(error) { // Se usuário não existir, mostrar mensagem de erro
+    } catch(error) { 
       alert(error.message)
     }
   }
 
-  load() { // Função para carregar os dados de cada linha
-    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [] // Transformar string em dado | Criando array vazia para o entries.
-
+  load() {
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || []
   }
 
-  save() { // Salvar a página no localStorage
-    localStorage.setItem ("@github-favorites:", JSON.stringify(this.entries)) // Salva a página pra quando atualizar, não perder os dados
+  save() { 
+    localStorage.setItem ("@github-favorites:", JSON.stringify(this.entries))
   }
 
-  delete(user) { // Função para deletar o usuário da tabela
+  delete(user) { 
     const filteredEntries = this.entries
-      .filter(entry => entry.login !== user.login) // Se a lógica "filter" retornar falso, o usuário será removido da tabela.
+      .filter(entry => entry.login !== user.login)
       this.entries = filteredEntries
       this.update()
       this.save()
   }
-
 }
 
-// Criar classe para visualização e eventos do HTML
+
 export class favoritesView extends favorites {
   constructor(root) {
     super(root)
-    this.tbody = this.root.querySelector('table tbody') // Pega o elemento tbody do HTML
+    this.tbody = this.root.querySelector('table tbody') 
     this.update()
     this.onadd()
   }
 
-  onadd() { // Função para resgatar o valor do input ao clicar no botão
+  onadd() {
     const buttonSearch = this.root.querySelector(".search button")
     buttonSearch.onclick = () => {
-      const { value } = this.root.querySelector(".search input") // Desestruturação de código
+      const { value } = this.root.querySelector(".search input") 
       this.add(value)
     }
   }
 
-  update() { // Função para atualizar os dados da tabela
+  update() { 
     this.removeAllTr()
-    this.entries.forEach(user => { // Para cada usuário, alterar os dados
+    this.entries.forEach(user => {
       const row = this.createRow()
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`
@@ -77,26 +73,25 @@ export class favoritesView extends favorites {
       row.querySelector('.user span').innerText = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
-      row.querySelector('.remove').onclick = () => { // Evento do botaão de remover o usuário da tabela
-        const isOk = confirm("Tem certeza que deseja remover esse usuário?") // Pop-up de confirmação 
+      row.querySelector('.remove').onclick = () => { 
+        const isOk = confirm("Tem certeza que deseja remover esse usuário?")
         if(isOk) {
           this.delete(user)
         }
       }
 
-      this.tbody.append(row) // Criar linha com os novos dados 
+      this.tbody.append(row)
     })
-    
   }
 
-  removeAllTr() { // Remover todas as linhas da tabela
+  removeAllTr() {
     this.tbody.querySelectorAll('tr')
       .forEach((tr) => {
         tr.remove()
       })
   }
 
-  createRow() { // Criar a tag TR e inserir o HTML dentro dela.
+  createRow() {
     const tr = document.createElement('tr')
     tr.innerHTML = `
       <td class="user">
